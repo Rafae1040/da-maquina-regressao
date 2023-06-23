69d9b8ea2346dcbca5068f09aa0ad7c93c94bb0a
Rafael Marcelino de Carvalho <123484081+Rafae1040@users.noreply.github.com>
2023-06-23 16:22:39 -0300

    Update limpeza.py

--- limpeza.py
+++ limpeza.py
@@ -1,3 +1,5 @@
+# Descartando as colunas originais e mantendo apenas a variável resposta e as variáveis preditivas com o sufixo _std", _nom" e "_ord".
+
 penguim = penguim.drop(columns=["species","island", "bill_length_mm", "bill_depth_mm", "flipper_length_mm", "sex"], axis=1)
 
 penguim.head()
