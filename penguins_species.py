5630221ec991867db820371f64667761b6d7cd65
Rafael Marcelino de Carvalho <123484081+Rafae1040@users.noreply.github.com>
2023-06-23 16:12:14 -0300

    Update penguins_species.py

--- penguins_species.py
+++ penguins_species.py
@@ -4,4 +4,6 @@ with sns.axes_style('whitegrid'):
   
   plt.savefig('penguins_species.png')
 
-#A especie Adelle apresenta ser a mais numerosa e entre as especies ser a maior no atribuito profundidade do bico(bill_depth_mm). A especie Chinstrap se aproxima no atributo comprimento do bico( bill_length_mm), com a especie Gentoo. Ja a especie Gentoo em dois atributos tem uma consistencia maior entre as especies os atributos seria o comprimeto das nadadeiras(flipper_length_mm) e massa corporal(body_mass_g).
+# A especie Adelle apresenta ser a mais numerosa e entre as especies ser a maior no atribuito profundidade do bico(bill_depth_mm). 
+# A especie Chinstrap se aproxima no atributo comprimento do bico( bill_length_mm), com a especie Gentoo. 
+# Ja a especie Gentoo em dois atributos tem uma consistencia maior entre as especies os atributos seria o comprimeto das nadadeiras(flipper_length_mm) e massa corporal(body_mass_g).
