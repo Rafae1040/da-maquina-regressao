d30da82cd5b3a2b9843c535a1058ced60f0ea8d0
Rafael Marcelino de Carvalho <123484081+Rafae1040@users.noreply.github.com>
2023-06-23 16:11:36 -0300

    Update penguins_sexo.py

--- penguins_sexo.py
+++ penguins_sexo.py
@@ -5,4 +5,7 @@ with sns.axes_style('whitegrid'):
   plt.savefig('penguins_sexo.png')
 
 
-# Os machos tendem a ser maiores e mais pesados do que as fêmeas. Isso significa que os machos geralmente têm medidas corporais maiores do que as fêmeas, como o comprimento do bico (bill_length_mm) ou o comprimento das nadadeiras (flipper_length_mm). No entanto, também é comum que haja uma variação natural nessas medidas entre os pinguins.
+# Os machos tendem a ser maiores e mais pesados do que as fêmeas. 
+# Isso significa que os machos geralmente têm medidas corporais maiores do que as fêmeas. 
+# Como o comprimento do bico (bill_length_mm) ou o comprimento das nadadeiras (flipper_length_mm). 
+# No entanto, também é comum que haja uma variação natural nessas medidas entre os pinguins.
