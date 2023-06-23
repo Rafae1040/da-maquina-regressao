7bc5224e92c6dda0b784f61c7805a21876d8d0e3
Rafael Marcelino de Carvalho <123484081+Rafae1040@users.noreply.github.com>
2023-06-23 16:13:38 -0300

    Update penguins_island.py

--- penguins_island.py
+++ penguins_island.py
@@ -4,4 +4,6 @@ with sns.axes_style('whitegrid'):
   
   plt.savefig('penguins_island.png')
 
-#A ilha Biscoe é aquela em que os pinguins apresentam a maior massa corporal(body_mass_g) e o maior comprimeto das nadadeiras( flipper_length_mm) essa ilha pode ter uma predominancia de habitar a especie Gentoo. Já a ilha Dream no atributo comprimento do bico(bill_depth_mm) se iguala e predominancia de habitar a especie Adelle. A ilha Torgensen apesar de ter indices a baixo das outras duas ilhas, no atributos podemos afirmar que essa ilha tem uma diversificação de especies por cada atributo.
+# A ilha Biscoe é aquela em que os pinguins apresentam a maior massa corporal(body_mass_g) e o maior comprimeto das nadadeiras( flipper_length_mm) essa ilha pode ter uma predominancia de habitar a especie Gentoo. 
+# Já a ilha Dream no atributo comprimento do bico(bill_depth_mm) se iguala e predominancia de habitar a especie Adelle. 
+# A ilha Torgensen apesar de ter indices a baixo das outras duas ilhas, no atributos podemos afirmar que essa ilha tem uma diversificação de especies por cada atributo.
